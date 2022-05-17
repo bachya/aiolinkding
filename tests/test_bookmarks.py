0854438225236f643dc864792daf1b8bfdcc470d
Aaron Bach <bachya1208@gmail.com>
2022-05-17 17:48:48 -0600

    Add API call to update an existing bookmark

--- tests/test_bookmarks.py
+++ tests/test_bookmarks.py
@@ -7,75 +7,11 @@ import pytest
 
 from aiolinkding import Client
 
-from .common import TEST_TOKEN, TEST_URL, load_fixture
+from .common import TEST_TOKEN, TEST_URL
 
 
 @pytest.mark.asyncio
-async def test_bookmarks_all(aresponses, bookmarks_async_get_all_response):
-    """Test getting all bookmarks."""
-    aresponses.add(
-        "127.0.0.1:8000",
-        "/api/bookmarks/",
-        "get",
-        aresponses.Response(
-            text=json.dumps(bookmarks_async_get_all_response),
-            status=200,
-            headers={"Content-Type": "application/json"},
-        ),
-    )
-
-    async with aiohttp.ClientSession() as session:
-        client = Client(TEST_URL, TEST_TOKEN, session=session)
-        # Include limit to exercise the inclusion of request parameters:
-        bookmarks = await client.bookmarks.async_get_all(limit=100)
-        assert bookmarks == bookmarks_async_get_all_response
-
-
-@pytest.mark.asyncio
-async def test_bookmarks_all_no_explicit_session(
-    aresponses, bookmarks_async_get_all_response
-):
-    """Test getting all bookmarks without an explicit ClientSession."""
-    aresponses.add(
-        "127.0.0.1:8000",
-        "/api/bookmarks/",
-        "get",
-        aresponses.Response(
-            text=json.dumps(bookmarks_async_get_all_response),
-            status=200,
-            headers={"Content-Type": "application/json"},
-        ),
-    )
-
-    client = Client(TEST_URL, TEST_TOKEN)
-    assert client._session is None
-
-    bookmarks = await client.bookmarks.async_get_all()
-    assert bookmarks == bookmarks_async_get_all_response
-
-
-@pytest.mark.asyncio
-async def test_bookmarks_archived(aresponses, bookmarks_async_get_archived_response):
-    """Test getting archived bookmarks."""
-    aresponses.add(
-        "127.0.0.1:8000",
-        "/api/bookmarks/archived/",
-        "get",
-        aresponses.Response(
-            text=json.dumps(bookmarks_async_get_archived_response),
-            status=200,
-            headers={"Content-Type": "application/json"},
-        ),
-    )
-
-    async with aiohttp.ClientSession() as session:
-        client = Client(TEST_URL, TEST_TOKEN, session=session)
-        archived_bookmarks = await client.bookmarks.async_get_archived()
-        assert archived_bookmarks == bookmarks_async_get_archived_response
-
-
-@pytest.mark.asyncio
-async def test_bookmarks_create(aresponses, bookmarks_async_get_single_response):
+async def test_create(aresponses, bookmarks_async_get_single_response):
     """Test creating a single bookmark."""
     aresponses.add(
         "127.0.0.1:8000",
@@ -103,7 +39,71 @@ async def test_bookmarks_create(aresponses, bookmarks_async_get_single_response)
 
 
 @pytest.mark.asyncio
-async def test_bookmarks_get(aresponses, bookmarks_async_get_single_response):
+async def test_get_all(aresponses, bookmarks_async_get_all_response):
+    """Test getting all bookmarks."""
+    aresponses.add(
+        "127.0.0.1:8000",
+        "/api/bookmarks/",
+        "get",
+        aresponses.Response(
+            text=json.dumps(bookmarks_async_get_all_response),
+            status=200,
+            headers={"Content-Type": "application/json"},
+        ),
+    )
+
+    async with aiohttp.ClientSession() as session:
+        client = Client(TEST_URL, TEST_TOKEN, session=session)
+        # Include limit to exercise the inclusion of request parameters:
+        bookmarks = await client.bookmarks.async_get_all(limit=100)
+        assert bookmarks == bookmarks_async_get_all_response
+
+
+@pytest.mark.asyncio
+async def test_get_all_no_explicit_session(
+    aresponses, bookmarks_async_get_all_response
+):
+    """Test getting all bookmarks without an explicit ClientSession."""
+    aresponses.add(
+        "127.0.0.1:8000",
+        "/api/bookmarks/",
+        "get",
+        aresponses.Response(
+            text=json.dumps(bookmarks_async_get_all_response),
+            status=200,
+            headers={"Content-Type": "application/json"},
+        ),
+    )
+
+    client = Client(TEST_URL, TEST_TOKEN)
+    assert client._session is None
+
+    bookmarks = await client.bookmarks.async_get_all()
+    assert bookmarks == bookmarks_async_get_all_response
+
+
+@pytest.mark.asyncio
+async def test_get_archived(aresponses, bookmarks_async_get_archived_response):
+    """Test getting archived bookmarks."""
+    aresponses.add(
+        "127.0.0.1:8000",
+        "/api/bookmarks/archived/",
+        "get",
+        aresponses.Response(
+            text=json.dumps(bookmarks_async_get_archived_response),
+            status=200,
+            headers={"Content-Type": "application/json"},
+        ),
+    )
+
+    async with aiohttp.ClientSession() as session:
+        client = Client(TEST_URL, TEST_TOKEN, session=session)
+        archived_bookmarks = await client.bookmarks.async_get_archived()
+        assert archived_bookmarks == bookmarks_async_get_archived_response
+
+
+@pytest.mark.asyncio
+async def test_get_single(aresponses, bookmarks_async_get_single_response):
     """Test getting a single bookmark."""
     aresponses.add(
         "127.0.0.1:8000",
@@ -120,3 +120,32 @@ async def test_bookmarks_get(aresponses, bookmarks_async_get_single_response):
         client = Client(TEST_URL, TEST_TOKEN, session=session)
         single_bookmark = await client.bookmarks.async_get_single(1)
         assert single_bookmark == bookmarks_async_get_single_response
+
+
+@pytest.mark.asyncio
+async def test_update(aresponses, bookmarks_async_get_single_response):
+    """Test creating a single bookmark."""
+    aresponses.add(
+        "127.0.0.1:8000",
+        "/api/bookmarks/1/",
+        "put",
+        aresponses.Response(
+            text=json.dumps(bookmarks_async_get_single_response),
+            status=200,
+            headers={"Content-Type": "application/json"},
+        ),
+    )
+
+    async with aiohttp.ClientSession() as session:
+        client = Client(TEST_URL, TEST_TOKEN, session=session)
+        updated_bookmark = await client.bookmarks.async_update(
+            1,
+            url="https://example.com",
+            title="Example title",
+            description="Example description",
+            tag_names=[
+                "tag1",
+                "tag2",
+            ],
+        )
+        assert updated_bookmark == bookmarks_async_get_single_response
