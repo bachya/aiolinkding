7f501f56c680642896dbb8bf589a7788f752c301
Aaron Bach <bachya1208@gmail.com>
2022-05-17 16:18:05 -0600

    Tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,7 +4,7 @@ import aiohttp
 import pytest
 
 from aiolinkding import Client
-from aiolinkding.errors import InvalidTokenError
+from aiolinkding.errors import InvalidTokenError, RequestError
 
 from .common import TEST_TOKEN, TEST_URL, load_fixture
 
@@ -25,5 +25,25 @@ async def test_invalid_token(aresponses):
 
     async with aiohttp.ClientSession() as session:
         with pytest.raises(InvalidTokenError):
+            client = Client(TEST_URL, TEST_TOKEN, session=session)
+            await client.async_request("get", "/api/whatever/")
+
+
+@pytest.mark.asyncio
+async def test_request_error(aresponses):
+    """Test an API call with a general request error."""
+    aresponses.add(
+        "127.0.0.1:8000",
+        "/api/whatever/",
+        "get",
+        aresponses.Response(
+            text=load_fixture("invalid_response.json"),
+            status=400,
+            headers={"Content-Type": "application/json"},
+        ),
+    )
+
+    async with aiohttp.ClientSession() as session:
+        with pytest.raises(RequestError):
             client = Client(TEST_URL, TEST_TOKEN, session=session)
             await client.async_request("get", "/api/whatever/")
