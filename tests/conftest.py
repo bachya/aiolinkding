db690be9369652c27b9af3db07b9e11155d7f5f5
Aaron Bach <bachya1208@gmail.com>
2022-05-20 11:38:59 -0600

    Use proper scope for JSON fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,31 +7,31 @@ import pytest
 from tests.common import load_fixture
 
 
-@pytest.fixture(name="bookmarks_async_get_all_response")
+@pytest.fixture(name="bookmarks_async_get_all_response", scope="session")
 def bookmarks_async_get_all_response_fixture():
     """Define a fixture to return all bookmarks."""
     return json.loads(load_fixture("bookmarks_async_get_all_response.json"))
 
 
-@pytest.fixture(name="bookmarks_async_get_archived_response")
+@pytest.fixture(name="bookmarks_async_get_archived_response", scope="session")
 def bookmarks_async_get_archived_response_fixture():
     """Define a fixture to return all archived bookmarks."""
     return json.loads(load_fixture("bookmarks_async_get_archived_response.json"))
 
 
-@pytest.fixture(name="bookmarks_async_get_single_response")
+@pytest.fixture(name="bookmarks_async_get_single_response", scope="session")
 def bookmarks_async_get_single_response_fixture():
     """Define a fixture to return a single bookmark."""
     return json.loads(load_fixture("bookmarks_async_get_single_response.json"))
 
 
-@pytest.fixture(name="tags_async_get_all_response")
+@pytest.fixture(name="tags_async_get_all_response", scope="session")
 def tags_async_get_all_response_fixture():
     """Define a fixture to return all tags."""
     return json.loads(load_fixture("tags_async_get_all_response.json"))
 
 
-@pytest.fixture(name="tags_async_get_single_response")
+@pytest.fixture(name="tags_async_get_single_response", scope="session")
 def tags_async_get_single_response_fixture():
     """Define a fixture to return a single tag."""
     return json.loads(load_fixture("tags_async_get_single_response.json"))
